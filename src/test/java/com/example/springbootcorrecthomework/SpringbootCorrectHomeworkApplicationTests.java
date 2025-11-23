a1224778efec4fa37ef0764531b98b7e86cf7739
LinZhaoYang <993773790@qq.com>
2025-11-23 13:16:34 +0800

    第一次提交

---
+++ src/test/java/com/example/springbootcorrecthomework/SpringbootCorrectHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcorrecthomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCorrectHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
